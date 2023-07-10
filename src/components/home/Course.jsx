a24d04d1a820729890663326f4e8281b54e2fe5d
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-07-10 19:00:26 +0600

    some edit

--- src/components/home/Course.jsx
+++ src/components/home/Course.jsx
@@ -1,39 +1,75 @@
-
+import { AiFillHtml5 } from "react-icons/ai";
+import { FaCss3Alt, FaGitSquare } from "react-icons/fa";
+import {
+  BiLogoJavascript,
+  BiLogoReact,
+  BiLogoPython,
+  BiLogoSass,
+} from "react-icons/bi";
+import { SiTypescript } from "react-icons/si";
 
 const Course = () => {
-    return (
-      <div className="p-6">
-        <h1 className="text-3xl mb-6">
-          Choose what to learn
-        </h1>
-        <div className="grid md:grid-cols-2 items-center lg:grid-cols-4 gap-2">
-          <div className="border-2 p-5 shadow-sm">
-            <h1 className="text-2xl">Learn html</h1>
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl mb-6">Choose what to learn</h1>
+      <div className="grid md:grid-cols-2 items-center lg:grid-cols-4 gap-2">
+        <div className="flex items-center text-3xl border-2 p-5 shadow-sm gap-2">
+          <div className="bg-[#0556f3] text-white rounded-full p-2">
+            <AiFillHtml5 />
           </div>
-          <div className="border-2 p-5 shadow-sm">
-            <h1 className="text-2xl">Learn css</h1>
+          <h1 className="text-2xl">Learn html</h1>
+        </div>
+        <div className="flex items-center text-3xl border-2 p-5 shadow-sm gap-2">
+          <div className="bg-[#0556f3] text-white rounded-full p-2">
+            <FaCss3Alt />
           </div>
-          <div className="border-2 p-5 shadow-sm">
-            <h1 className="text-2xl">Learn JavaScript</h1>
+          <h1 className="text-2xl">Learn css</h1>
+        </div>
+        <div className="flex items-center text-3xl border-2 p-5 shadow-sm gap-2">
+          <div className="bg-[#0556f3] text-white rounded-full p-2">
+            <BiLogoJavascript />
           </div>
-          <div className="border-2 p-5 shadow-sm">
-            <h1 className="text-2xl">Learn css</h1>
+
+          <h1 className="text-2xl">Learn JavaScript</h1>
+        </div>
+        <div className="flex items-center text-3xl border-2 p-5 shadow-sm gap-2">
+          <div className="bg-[#0556f3] text-white rounded-full p-2">
+            <BiLogoReact />
           </div>
-          <div className="border-2 p-5 shadow-sm">
-            <h1 className="text-2xl">Learn JavaScript</h1>
+
+          <h1 className="text-2xl">Learn React</h1>
+        </div>
+        <div className="flex items-center text-3xl border-2 p-5 shadow-sm gap-2">
+          <div className="bg-[#0556f3] text-white rounded-full p-2">
+            <BiLogoPython />
           </div>
-          <div className="border-2 p-5 shadow-sm">
-            <h1 className="text-2xl">Learn css</h1>
+
+          <h1 className="text-2xl">Learn Pythone</h1>
+        </div>
+        <div className="flex items-center text-3xl border-2 p-5 shadow-sm gap-2">
+          <div className="bg-[#0556f3] text-white rounded-full p-2">
+            <SiTypescript />
           </div>
-          <div className="border-2 p-5 shadow-sm">
-            <h1 className="text-2xl">Learn JavaScript</h1>
+
+          <h1 className="text-2xl">Learn TypeScript</h1>
+        </div>
+        <div className="flex items-center text-3xl border-2 p-5 shadow-sm gap-2">
+          <div className="bg-[#0556f3] text-white rounded-full p-2">
+            <BiLogoSass />
           </div>
-          <div className="border-2 p-5 shadow-sm">
-            <h1 className="text-2xl">Learn JavaScript</h1>
+
+          <h1 className="text-2xl">Learn Sass</h1>
+        </div>
+        <div className="flex items-center text-3xl border-2 p-5 shadow-sm gap-2">
+          <div className="bg-[#0556f3] text-white rounded-full p-2">
+            <FaGitSquare />
           </div>
+
+          <h1 className="text-2xl">Learn Git</h1>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default Course;
--- src/components/home/WhyMozahidWorld.jsx
+++ src/components/home/WhyMozahidWorld.jsx
@@ -1,4 +1,7 @@
 
+import { FaStarOfDavid } from "react-icons/fa";
+import { BsCodeSquare } from "react-icons/bs";
+import { AiOutlineQrcode } from "react-icons/ai";
 
 const WhyMozahidWorld = () => {
     return (
@@ -8,8 +11,21 @@ const WhyMozahidWorld = () => {
         </h1>
         <div className="grid p-6 md:grid-cols-2 justify-center items-center lg:grid-cols-3 ">
           <div className="flex justify-center mt-6">
-            <div className="p-2">
-              <img src="https://i.ibb.co/6bGvBfV/instagram.png" alt="" />
+            <div className="p-2 text-4xl text-[#0556f3]">
+              <FaStarOfDavid />
+            </div>
+            <div className="">
+              <h1 className=" mb-2 text-2xl">Programming made easy</h1>
+              <p>
+                We focus on simplicity.<br></br> Programming tutorials and{" "}
+                <br></br>examples written in simple,<br></br> understandable
+                language for <br></br>beginners.
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-center mt-6 ">
+            <div className="p-2 text-4xl text-[#0556f3]">
+              <AiOutlineQrcode />
             </div>
             <div className="">
               <h1 className=" mb-2 text-2xl">Content You Can Trust</h1>
@@ -21,24 +37,11 @@ const WhyMozahidWorld = () => {
             </div>
           </div>
           <div className="flex justify-center mt-6">
-            <div className="p-2">
-              <img src="https://i.ibb.co/6bGvBfV/instagram.png" alt="" />
+            <div className="p-2 text-4xl text-[#0556f3]">
+              <BsCodeSquare />
             </div>
             <div className="">
-              <h1 className=" mb-2 text-2xl">Content You Can Trust</h1>
-              <p>
-                We focus on simplicity.<br></br> Programming tutorials and{" "}
-                <br></br>examples written in simple,<br></br> understandable
-                language for <br></br>beginners.
-              </p>
-            </div>
-          </div>
-          <div className="flex justify-center mt-6">
-            <div className="p-2">
-              <img src="https://i.ibb.co/6bGvBfV/instagram.png" alt="" />
-            </div>
-            <div className="">
-              <h1 className=" mb-2 text-2xl">Content You Can Trust</h1>
+              <h1 className=" mb-2 text-2xl">Learn by Doing</h1>
               <p>
                 We focus on simplicity.<br></br> Programming tutorials and{" "}
                 <br></br>examples written in simple,<br></br> understandable
