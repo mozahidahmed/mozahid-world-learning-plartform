79db405b794f63d23cb9bdd4b3655dff279b2da4
mozahidsani <mozahidwebmin11d10@gmail.com>
2023-07-20 12:44:49 +0600

    done

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
         <div className="py-2 flex  justify-between items-center lg:gap-8 ">
           <div className="flex gap-2  items-center ">
             <GiWorld className="lg:text-4xl md:text-2xl text-xl text-[#0556f3]" />
-            <h1 className="lg:text-3xl md:text-3xl text-xl">Mozahid_World</h1>
+            <h1 className="lg:text-3xl md:text-3xl text-xl">M_World</h1>
           </div>
           <div className="flex lg:gap-6 md:gap-4 gap-4  items-center text-2xl lg:mt-0 md:mt-0 ">
             <h1 className="lg:text-2xl  text-xl">Home</h1>
